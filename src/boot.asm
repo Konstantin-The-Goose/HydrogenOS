d595c39d461ca7a628a3e267470172c586ef7bfa
KonstantinTheGoose <iamaagoose@outlook.com>
2026-04-20 17:24:17 +0100

    Enhance bootloader functionality and error handling
    Refactor bootloader to save boot drive and improve kernel loading.

--- src/boot.asm
+++ src/boot.asm
@@ -1,32 +1,37 @@
-org 0x7c00 ; bios loads the code at this address 
-bits 16 
+org 0x7c00
+bits 16
 
-main: 
-	; set up data segments
-	mov ax, 0 ; cant write to ds/es directly
-	mov ds, ax
-	mov es, ax
+main:
+    ; save boot drive
+    mov [boot_drive], dl
 
-	; set up stack
-	mov ss, ax
-	mov sp, 0x7c00
+    ; set segments
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
 
-	; load kernel (1 sector after bootloader)
-	 mov ah, 0x02        ; BIOS read
-	 mov al, 1           ; number of sectors
-	 mov ch, 0           ; cylinder
-	 mov cl, 2           ; sector (1 = bootloader, so kernel = 2)
-	 mov dh, 0           ; head
-	 mov dl, 0x80        ; drive (hard disk)
-	 mov bx, 0x1000      ; load address
-	 int 0x13            ; BIOS disk read
-	 jmp 0x0000:0x1000   ; jump to kernel
+    ; stack
+    mov ss, ax
+    mov sp, 0x7c00
 
-hang:
-	jmp hang	
+    ; load kernel to 0x1000
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, [boot_drive]
+    mov bx, 0x1000
+    int 0x13
+    jc disk_error
 
-times 510-($-$$) db 0 ; gives size of the program (in bytes)
-dw 0xAA55 
+    ; jump to kernel
+    jmp 0x0000:0x1000
 
-; cant wait to be free from ts and write a kernel in c
+disk_error:
+    jmp $
 
+boot_drive db 0
+
+times 510-($-$$) db 0
+dw 0xAA55
